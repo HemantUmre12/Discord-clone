1675f694005a56396baef8dbc8357c729126767b
Hemant Umre <hemantumre12@gmail.com>
2022-03-21 01:47:45 +0530

    Added feature to change themes using tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 const colors = require('tailwindcss/colors')
 
+function withOpacity(cssVariable) {
+  return ({ opacityValue }) => {
+      return `rgba(var(${cssVariable}), ${opacityValue})`
+  }
+}
+
 module.exports = {
   mode: 'jit',
   content: ['./public/**/*.{html,js}'],
@@ -17,6 +23,23 @@ module.exports = {
           200: '#ebedef',
           100: '#f2f3f5',
         },
+      },
+      textColor: {
+        skin: {
+          base: 'var(--color-text-base)',
+          muted: 'var(--color-text-muted)',
+          'main-theme': 'var(--color-main-theme)',
+        }
+      },
+      backgroundColor: {
+        skin: {
+          muted: 'var(--color-text-muted)',
+          'primary': 'var(--color-primary-bg)',
+          'secondary': 'var(--color-secondary-bg)',
+          'tertiary': 'var(--color-tertiary-bg)',
+          'main-theme': 'var(--color-main-theme)',
+          'text-area': 'var(--color-textarea-bg)',
+        }
       },
     },
   },
